68630c6bed332561ad739f11b87fd5d1cc91f314
Moiseis Cruz <moizzito@gmail.com>
2024-07-05 11:42:28 -0300

    Criei o arquivo .js e busquei alguns elementos.

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+let text = document.getElementById("text");
+
+let bntCriptografar = document.getElementById("btn-criptografar");
+
+let bthDescriptografar = document.getElementById("btn-descriptografar");
