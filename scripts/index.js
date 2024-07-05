3bcaa20a751f93eb1b929f0918633fd8afd7afa8
Moiseis Cruz <moizzito@gmail.com>
2024-07-05 11:51:39 -0300

    Criei os eventos dos botões.

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,21 @@ let text = document.getElementById("text");
 
 let bntCriptografar = document.getElementById("btn-criptografar");
 
-let bthDescriptografar = document.getElementById("btn-descriptografar");
+let btnDescriptografar = document.getElementById("btn-descriptografar");
 
 
 let output = document.getElementById("output");
 
+
+
+bntCriptografar.addEventListener("click", () => {
+    let textValue = text.value;
+
+    output.innerHTML = textValue;
+});
+
+btnDescriptografar.addEventListener("click", () => {
+    let textValue = text.value;
+
+    output.innerHTML = textValue;
+})
