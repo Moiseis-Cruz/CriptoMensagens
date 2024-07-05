74aca2799d9b7add1a300ae0314397ec81c554d9
Moiseis Cruz <moizzito@gmail.com>
2024-07-05 12:00:36 -0300

    Criei a função para criptografar texto.

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,20 @@ let btnDescriptografar = document.getElementById("btn-descriptografar");
 
 let output = document.getElementById("output");
 
-
+function criptografarText(text) {
+    return text.replace(/e/g, "enter")
+                .replace(/i/g, "imes")
+                .replace(/a/g, "ai")
+                .replace(/o/g, "ober")
+                .replace(/u/g, "ufat")
+};
 
 bntCriptografar.addEventListener("click", () => {
     let textValue = text.value;
 
-    output.innerHTML = textValue;
+    let criptografado = criptografarText(textValue)
+
+    output.innerHTML = criptografado;
 });
 
 btnDescriptografar.addEventListener("click", () => {
